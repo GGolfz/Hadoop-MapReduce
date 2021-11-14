c25739f1cdfbd9124d7a7bc079dab866d4b5f559
GGolfz <wisarutgolf11@gmail.com>
2021-11-14 20:28:10 +0700

    add: configuration file :sparkles:

---
+++ hadoop-env.sh
@@ -0,0 +1,6 @@
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export HDFS_NAMENODE_USER="hadoop"
+export HDFS_DATANODE_USER="hadoop"
+export HDFS_SECONDARYNAMENODE_USER="hadoop"
+export YARN_RESOURCEMANAGER_USER="hadoop"
+export YARN_NODEMANAGER_USER="hadoop"
